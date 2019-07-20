0d2f51f8d031c0255f06877f6ec5ccaf9aa14cfa
taraxacum12 <45296313+taraxacum12@users.noreply.github.com>
2019-07-20 23:19:13 +0400

    sed

---
+++ DirstKidlLearning/Assets/gvrTel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gvrTel : MonoBehaviour {
+
+    public int dstOfRay = 10;
+    private RaycastHit hit;
+
+    private MagnetSensor mgsens;
+
+	void Start () {
+
+        mgsens = GetComponent<MagnetSensor>();
+
+	}
+	
+	
+	void Update () {
+
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+
+        if(Physics.Raycast(ray, out hit, dstOfRay))
+        {
+            if (Input.GetButtonDown("Fire1") && hit.transform.gameObject.tag == "Ground")
+            teleportPlayer(hit.point);
+        }
+	}
+
+    public void teleportPlayer(Vector3 target)
+    {
+        transform.position = new Vector3(target.x, transform.position.y, target.z);
+    }
+}
